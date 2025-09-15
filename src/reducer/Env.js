da8f9a1e2928e220766c77b404490f822134c2e9
vikramchvc <54850053+vikramchvc@users.noreply.github.com>
2025-09-15 11:36:03 +0530

    Update Env.js

--- src/reducer/Env.js
+++ src/reducer/Env.js
@@ -2,7 +2,7 @@ const devEnvironment = {
   name: 'development',
   frontendUrl: 'http://localhost:3000/',
   backendUrl: 'http://localhost:8000/',
-  googleClientId: '1074227815727-ppqu41nt51n3a4ijvd41icobjpiv0mbq.apps.googleusercontent.com',
+  googleClientId: '**************************************',
 };
 
 
